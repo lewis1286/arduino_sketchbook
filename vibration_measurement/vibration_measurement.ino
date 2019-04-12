b5387053aef8271a28cb1002f67c949f21d4f203
lewis1286 <lewisguignard@gmail.com>
2019-04-11 22:48:39 -0700

    changes!

--- vibration_measurement/vibration_measurement.ino
+++ vibration_measurement/vibration_measurement.ino
@@ -8,14 +8,14 @@
 //this must be a power of 2
 #define DATA_SIZE 256
 
-//sample rate
-#define FS 8000
+#define DL 1 // Duration of delay within loop
+//sample rate - with DL = 1, takes 2ms per loop, so FS = 1/.002 = 500 Hz
+#define FS 500
 
 /* Assign a unique ID to this sensor at the same time */
 Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(54321);
-
 #define NS 1000 // Number of samples to take in for a fft batch
-#define DL 5 // Duratoin of delay within loop
+
 
 void setup()
 {
@@ -81,8 +81,8 @@ float get_cos_sim(float initial[3], float finall[3]){
 void shape_array(float float_array[NS], q15_t int_array[NS]){
     /*cos array is in [-1,1] but we need it in ints for FFT*/
     for(int i=0; i<NS; i++){
-//        float temp = ((-1 * float_array[i]) + 1)* 1000;
-        float temp = float_array[i] * 1000;
+        float temp = ((-1 * float_array[i]) + 1)* 1000;
+//        float temp = float_array[i] * 1000;
         int_array[i] = (int) temp;
     }
 }
@@ -96,9 +96,12 @@ void loop()
     float old_vector[3] = {0.57735, 0.57735, 0.57735}; // unit vector
     float new_vector[3] = {0.0, 0.0, 0.0};
     float leng;
+    float start_time = micros();
 
-    SerialUSB.println(" starting a loop.. ");
+    float time_delta;
+
     for(int i=0; i<NS; i++){
+//        if(i==1){start_time = micros();}
         read_imu(new_vector);
         /* compute cosine similarity between new direction and old */
         if(i==0){cossim[i] = 1;}
@@ -115,32 +118,35 @@ void loop()
 
         /*sleep*/
         delay(DL);
-    }
-    SerialUSB.println(" done with a loop.. ");
-    shape_array(cossim, intcossim);
-    // print the float array of cosine similarity
-    SerialUSB.println("cossim: ");
-    for(int i=0; i<10; i++){
-        SerialUSB.print(cossim[i]);
-        SerialUSB.print(", ");
-    }
-    // print the integer array of cosine similarity
-    SerialUSB.println(" ");
-    SerialUSB.println("intcossim: ");
-    for(int i=0; i<10; i++){
-        SerialUSB.print(intcossim[i]);
-        SerialUSB.print(", ");
-    }
-    SerialUSB.println(' ');
 
+    }
+    float end_time = micros();
+    time_delta = end_time - start_time;
+    SerialUSB.print("Run-time: ");
+    SerialUSB.println(time_delta / NS);
+
+    shape_array(cossim, intcossim);
     /*[> Perform FFT on directions <]*/
     ZeroFFT(intcossim, DATA_SIZE);
+//    plot_fft(intcossim);
+    delay(2000);
+    SerialUSB.println(6);
+}
 
-    SerialUSB.println("intcossim post FFT: ");
-    // Did anything change on intcossim?
-    for(int i=0; i<10; i++){
-        SerialUSB.print(intcossim[i]);
-        SerialUSB.print(", ");
-    }
-    SerialUSB.println(' ');
+void print_fft(q15_t data[]){
+  for(int i=2; i<DATA_SIZE/2; i++){
+    
+    //print the frequency
+    SerialUSB.print(FFT_BIN(i, FS, DATA_SIZE));
+    SerialUSB.print(" Hz: ");
+
+    //print the corresponding FFT output
+    SerialUSB.println(data[i]);
+  }
+}
+void plot_fft(q15_t data[]){
+  for(int i=2; i<DATA_SIZE/2; i++){
+    //print the corresponding FFT output
+    SerialUSB.println(data[i]);
+  }
 }
